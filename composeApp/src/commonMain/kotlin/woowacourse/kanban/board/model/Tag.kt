ebdbfed4294a0635552381fa961102554e4ed417
aksworns22 <wkdeogks17@gmail.com>
2026-03-04 14:10:20 +0900

    feat: tag, tagGroup 구현 수정

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/model/Tag.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/model/Tag.kt
@@ -1,18 +1,12 @@
 package woowacourse.kanban.board.model
 
-sealed class Tag {
-    data class TagText private constructor(val text: String) : Tag() {
-        init {
-            require(text.length <= MAXIMUM_TAG_LENGTH) { "[ERROR] 태그 텍스트는 5글자 이하여야 합니다." }
-        }
-
-        companion object {
-            const val MAXIMUM_TAG_LENGTH = 5
-            fun of(text: String): Tag {
-                if(text.isNotEmpty()) return TagText(text)
-                return None
-            }
-        }
+data class Tag(val text: String) {
+    init {
+        require(text.isNotEmpty()) { "[ERROR] 태그 텍스트는 빈 값일 수 없습니다." }
+        require(text.length <= MAXIMUM_TAG_LENGTH) { "[ERROR] 태그 텍스트는 5글자 이하여야 합니다." }
     }
-    object None : Tag()
+
+    companion object {
+        const val MAXIMUM_TAG_LENGTH = 5
+    }
 }
--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/model/TagGroup.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/model/TagGroup.kt
@@ -1,11 +1,13 @@
 package woowacourse.kanban.board.model
 
-data class TagGroup(val tags: List<Tag>) {
-    init {
-        require(tags.filterIsInstance<Tag.TagText>().size <= MAXIMUM_TAG_COUNT) { "[ERROR] 태그는 최대 5개 입니다." }
+sealed class TagGroup {
+    data class Items(val tags: List<Tag>) : TagGroup() {
+        init {
+            require(tags.size <= MAXIMUM_TAG_COUNT) { "[ERROR] 태그 개수는 5개 이하여야합니다." }
+        }
+        companion object {
+            const val MAXIMUM_TAG_COUNT = 5
+        }
     }
-
-    companion object {
-        const val MAXIMUM_TAG_COUNT = 5
-    }
+    object None : TagGroup()
 }
