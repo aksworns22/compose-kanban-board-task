bafafae10b49b4b51faa912413e2cc1805f474fb
aksworns22 <wkdeogks17@gmail.com>
2026-03-04 14:08:44 +0900

    refactor: titleText를 title로 변경

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/model/Title.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/model/Title.kt
@@ -1,7 +1,7 @@
 package woowacourse.kanban.board.model
 
 @JvmInline
-value class TitleText(val text: String) {
+value class Title(val text: String) {
     init {
         require(text.isNotEmpty()) {"[ERROR] 제목은 빈 값일 수 없습니다." }
     }
