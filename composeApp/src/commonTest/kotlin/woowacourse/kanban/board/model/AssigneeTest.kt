61d083efdbc7fd94258411dd2f85402af0314544
aksworns22 <wkdeogks17@gmail.com>
2026-03-06 19:40:05 +0900

    refactor(assigneeTest): assertJ를 통한 테스트로 변경

--- composeApp/src/commonTest/kotlin/woowacourse/kanban/board/model/AssigneeTest.kt
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/model/AssigneeTest.kt
@@ -1,12 +1,13 @@
 package woowacourse.kanban.board.model
 
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import kotlin.test.Test
 import kotlin.test.assertFailsWith
 
 class AssigneeTest {
     @Test
     fun `담당자는 빈 텍스트일 수 없다`() {
-        assertFailsWith<IllegalArgumentException> {
+        assertThatThrownBy {
             Assignee("")
         }
     }
