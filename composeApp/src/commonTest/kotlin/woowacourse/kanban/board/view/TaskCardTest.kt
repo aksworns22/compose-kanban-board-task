36d470f863eae03e46ee7a45fd175bf30c32d3ee
aksworns22 <wkdeogks17@gmail.com>
2026-03-10 22:40:42 +0900

    refactor: 테스트 코드가 위에서 아래로 읽히도록 개선

--- composeApp/src/commonTest/kotlin/woowacourse/kanban/board/view/TaskCardTest.kt
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/view/TaskCardTest.kt
@@ -17,124 +17,115 @@ import woowacourse.kanban.board.model.Title
 class TaskCardTest {
     @Test
     fun `제목, 설명, 태그 그룹, 담당자가 모두 입력된 카드라면 화면에 모두 노출한다`() = runComposeUiTest {
-        val titleText = "LazyColumn 컴포넌트 구현"
-        val descriptionText = "세로 스크롤 가능한 리스트 컴포넌트를 만들고 성능 최적화를 적용"
-        val tagTextGroup = listOf("컴포넌트", "성능")
-        val assigneeName = "다이노"
-        val expectedTextGroup = listOf(titleText, descriptionText, assigneeName) + tagTextGroup
+        val task = Task(
+            title = Title(text = "LazyColumn 컴포넌트 구현"),
+            description = Description("세로 스크롤 가능한 리스트 컴포넌트를 만들고 성능 최적화를 적용"),
+            tagGroup = TagGroup(tags = listOf(Tag("컴포넌트"), Tag("성능"))),
+            assignee = Assignee(name = "다이노"),
+        )
 
         setContent {
-            TaskCard(
-                task = Task(
-                    title = Title(text = titleText),
-                    description = Description(descriptionText),
-                    tagGroup = TagGroup(tags = tagTextGroup.map { Tag(it) }),
-                    assignee = Assignee(name = assigneeName),
-                ),
-            )
+            TaskCard(task = task)
         }
 
-        for (expectedText in expectedTextGroup) {
-            onNodeWithText(expectedText).assertIsDisplayed()
+        val (title, description, tagGroup, assignee) = task
+        onNodeWithText(title.text).assertIsDisplayed()
+        onNodeWithText(description.text).assertIsDisplayed()
+        onNodeWithText(assignee.name).assertIsDisplayed()
+        tagGroup.tags.forEach { tag ->
+            onNodeWithText(tag.text).assertIsDisplayed()
         }
     }
 
     @Test
     fun `설명과 태그 그룹이 각각 빈 값으로 입력된 경우라면 제목, 담당자만 노출한다`() = runComposeUiTest {
-        val titleText = "LazyColumn 컴포넌트 구현"
-        val assigneeName = "다이노"
-        val expectedTextGroup = listOf(titleText, assigneeName)
+        val task = Task(
+            title = Title(text = "LazyColumn 컴포넌트 구현"),
+            description = Description.empty,
+            tagGroup = TagGroup(tags = listOf()),
+            assignee = Assignee(name = "다이노"),
+        )
 
         setContent {
-            TaskCard(
-                task = Task(
-                    title = Title(text = titleText),
-                    description = Description.empty,
-                    tagGroup = TagGroup(tags = listOf()),
-                    assignee = Assignee(name = assigneeName),
-                ),
-            )
+            TaskCard(task = task)
         }
 
+        val (title, _, _, assignee) = task
         onNodeWithTag("DescriptionText").assertDoesNotExist()
         onNodeWithTag("TagBadgeGroup").assertDoesNotExist()
-
-        for (expectedText in expectedTextGroup) {
-            onNodeWithText(expectedText).assertIsDisplayed()
-        }
+        onNodeWithText(title.text).assertIsDisplayed()
+        onNodeWithText(assignee.name).assertIsDisplayed()
     }
 
     @Test
     fun `설명이 빈 값으로 입력된 경우라면 제목, 태그, 담당자만 노출한다`() = runComposeUiTest {
-        val titleText = "LazyColumn 컴포넌트 구현"
-        val tagTextGroup = listOf("컴포넌트", "성능")
-        val assigneeName = "다이노"
-        val expectedTextGroup = listOf(titleText, assigneeName) + tagTextGroup
+        val task = Task(
+            title = Title(text = "LazyColumn 컴포넌트 구현"),
+            description = Description.empty,
+            tagGroup = TagGroup(tags = listOf(Tag("컴포넌트"), Tag("성능"))),
+            assignee = Assignee(name = "다이노"),
+        )
 
         setContent {
-            TaskCard(
-                task = Task(
-                    title = Title(text = titleText),
-                    description = Description.empty,
-                    tagGroup = TagGroup(tags = tagTextGroup.map { Tag(it) }),
-                    assignee = Assignee(name = assigneeName),
-                ),
-            )
+            TaskCard(task = task)
         }
 
+        val (title, _, tagGroup, assignee) = task
         onNodeWithTag("DescriptionText").assertDoesNotExist()
-
-        for (expectedText in expectedTextGroup) {
-            onNodeWithText(expectedText).assertIsDisplayed()
+        onNodeWithText(title.text).assertIsDisplayed()
+        onNodeWithText(assignee.name).assertIsDisplayed()
+        tagGroup.tags.forEach { tag ->
+            onNodeWithText(tag.text).assertIsDisplayed()
         }
     }
 
     @Test
     fun `태그 그룹이 빈 값으로 입력된 경우라면 제목, 설명, 담당자만 노출한다`() = runComposeUiTest {
-        val titleText = "LazyColumn 컴포넌트 구현"
-        val descriptionText = "세로 스크롤 가능한 리스트 컴포넌트를 만들고 성능 최적화를 적용"
-        val assigneeName = "다이노"
-        val expectedTextGroup = listOf(titleText, assigneeName)
+        val task = Task(
+            title = Title(text = "LazyColumn 컴포넌트 구현"),
+            description = Description("세로 스크롤 가능한 리스트 컴포넌트를 만들고 성능 최적화를 적용"),
+            tagGroup = TagGroup(tags = listOf()),
+            assignee = Assignee(name = "다이노"),
+        )
 
         setContent {
-            TaskCard(
-                task = Task(
-                    title = Title(text = titleText),
-                    description = Description(descriptionText),
-                    tagGroup = TagGroup(tags = listOf()),
-                    assignee = Assignee(name = assigneeName),
-                ),
-            )
+            TaskCard(task = task)
         }
 
+        val (title, description, _, assignee) = task
         onNodeWithTag("TagBadgeGroup").assertDoesNotExist()
-
-        for (expectedText in expectedTextGroup) {
-            onNodeWithText(expectedText).assertIsDisplayed()
-        }
+        onNodeWithText(title.text).assertIsDisplayed()
+        onNodeWithText(description.text).assertIsDisplayed()
+        onNodeWithText(assignee.name).assertIsDisplayed()
     }
 
     @Test
     fun `태스크 카드의 태그 그룹은 태그를 최대 5개까지만 노출한다`() = runComposeUiTest {
-        val visibleTagText = listOf("tag1", "tag2", "tag3", "tag4", "tag5")
-        val invisibleTagText = "tag6"
-        val tagTextGroup = visibleTagText + invisibleTagText
+        val tagGroup = TagGroup(
+            tags = listOf(
+                Tag("tag1"),
+                Tag("tag2"),
+                Tag("tag3"),
+                Tag("tag4"),
+                Tag("tag5"),
+                Tag("tag6"),
+            ),
+        )
 
         setContent {
             TaskCard(
                 task = Task(
                     title = Title(text = "LazyColumn 컴포넌트 구현"),
                     description = Description("세로 스크롤 가능한 리스트 컴포넌트를 만들고 성능 최적화를 적용"),
-                    tagGroup = TagGroup(tags = tagTextGroup.map { Tag(it) }),
+                    tagGroup = tagGroup,
                     assignee = Assignee(name = "다이노"),
                 ),
             )
         }
 
-        onNodeWithText(invisibleTagText).assertDoesNotExist()
-
-        for (tagText in visibleTagText) {
-            onNodeWithText(tagText).assertIsDisplayed()
+        tagGroup.tags.take(5).forEach { tag ->
+            onNodeWithText(tag.text).assertIsDisplayed()
         }
+        onNodeWithText(tagGroup.tags[5].text).assertDoesNotExist()
     }
 }
