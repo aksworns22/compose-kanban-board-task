a084514d45d944f95a197b3e2f4bcfba556d95cb
aksworns22 <wkdeogks17@gmail.com>
2026-03-10 19:06:05 +0900

    test(title): 성공 테스트 추가

--- composeApp/src/commonTest/kotlin/woowacourse/kanban/board/model/TitleTest.kt
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/model/TitleTest.kt
@@ -1,6 +1,7 @@
 package woowacourse.kanban.board.model
 
 import kotlin.test.Test
+import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 
 class TitleTest {
@@ -9,5 +10,11 @@ class TitleTest {
         assertThatThrownBy {
             Title("")
         }.isInstanceOf(IllegalArgumentException::class.java)
+    }
+
+    @Test
+    fun `제목이 빈 텍스트가 아니라면 생성할 수 있다`() {
+        assertThat(Title("근사한 제목").text)
+            .isEqualTo("근사한 제목")
     }
 }
